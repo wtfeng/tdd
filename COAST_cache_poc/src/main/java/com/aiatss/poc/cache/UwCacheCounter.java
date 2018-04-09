57b5eaf835f5f81436a2b04ff3000d8e9beccda3
v <a@a.com>
2018-04-09 18:56:17 +0800

    del

--- COAST_cache_poc/src/main/java/com/aiatss/poc/cache/UwCacheCounter.java
+++
@@ -1,9 +0,0 @@
-package com.aiatss.poc.cache;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class UwCacheCounter {
-	public int USING_CACHE_COUNT = 0;
-	public int QUERY_DB_COUNT = 0;
-}
